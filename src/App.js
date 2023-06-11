a91ecf6c8060699ddfb7022ec8022c3faef684ee
JacobWillis <jacobwillis2012@gmail.com>
2023-06-11 17:53:42 -0400

    'added header and navigation.js'

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Header from './components/header';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'animate.css/animate.min.css';
+
 // import Navigation from './components/navigation';
 // import Content from './components/content';
 // import Footer from './components/footer';
--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,20 @@
 // import React, { useState } from 'react';
+import Navigation from './navigation';
+import { AiFillGithub, AiFillLinkedin, AiFillTwitterCircle } from 'react-icons/ai';
 
 const Header = () => {
+return (
     <header>
-        My Portfolio
+        <Navigation />
+        <div>    
+            <AiFillGithub />   
+            <AiFillLinkedin />
+            <AiFillTwitterCircle />
+        </div>
+        <h1>Jacob Willis</h1>
+        <h2>Full-Stack Developer</h2>
     </header>
+    )
 };
 
 export default Header;
