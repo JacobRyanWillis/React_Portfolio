836dd06abc369bc7dee420f1142dff21b5a724a9
JacobWillis <jacobwillis2012@gmail.com>
2023-06-11 14:31:51 -0400

    'setup file structure for

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './header';
+import Navigation from './navigation';
+import Content from './content';
+import Footer from './footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Navigation />
+      <Content />
+      <Footer />
     </div>
   );
 }
