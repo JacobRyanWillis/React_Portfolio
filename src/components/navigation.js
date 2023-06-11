6d186ab2319585d851030a52052a81ce66d08323
JacobWillis <jacobwillis2012@gmail.com>
2023-06-11 17:53:42 -0400

    'added header and navigation.js'

---
+++ src/components/navigation.js
@@ -0,0 +1,15 @@
+// import React, { useState } from 'react';
+
+const Navigation = () => {
+    return (
+    <nav>
+        <ul>
+            <li><a href='#about'>About</a></li>
+            <li><a href='#projects'>Projects</a></li>
+            <li><a href='#contacts'>Contact Me</a></li>
+        </ul>
+    </nav>
+    ) 
+}
+
+export default Navigation;
