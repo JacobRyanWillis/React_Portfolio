74635a79096aef6a302bc9c0eb409b49f3350e62
JacobWillis <jacobwillis2012@gmail.com>
2023-06-11 23:09:02 -0400

    'added footer and header with CSS'

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,15 +1,31 @@
 // import React, { useState } from 'react';
+import "./navigation.css";
+// import { FaReact } from "react-icons/fa";
 
 const Navigation = () => {
-    return (
-    <nav>
-        <ul>
-            <li><a href='#about'>About</a></li>
-            <li><a href='#projects'>Projects</a></li>
-            <li><a href='#contacts'>Contact Me</a></li>
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <div className="navbar-brand">
+          <h2 className="name">Jacob Willis</h2>
+          <span className="role">Full Stack Developer</span>
+        </div>
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <a className="nav-link" href="#about">About</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#projects">Projects</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#contact">Contact</a>
+          </li>
         </ul>
+      </div>
     </nav>
-    ) 
-}
+  );
+};
 
 export default Navigation;
+
+
