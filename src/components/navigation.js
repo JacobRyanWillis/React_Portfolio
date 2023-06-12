0263b0c9be3a0cbf349ddae485629a731a88614c
JacobWillis <jacobwillis2012@gmail.com>
2023-06-11 23:35:09 -0400

    'added several components for content.js'

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -2,30 +2,45 @@
 import "./navigation.css";
 // import { FaReact } from "react-icons/fa";
 
-const Navigation = () => {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container">
-        <div className="navbar-brand">
-          <h2 className="name">Jacob Willis</h2>
-          <span className="role">Full Stack Developer</span>
+const Navigation = ({ activeSection, handleNavigationClick }) => {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+          <div className="navbar-brand">
+            <h2 className="name">Jacob Willis</h2>
+            <span className="role">Full Stack Developer</span>
+          </div>
+          <ul className="navbar-nav ml-auto">
+            <li
+              className={`nav-item ${activeSection === 'About' ? 'active' : ''}`}
+              onClick={() => handleNavigationClick('About')}
+            >
+              <a className="nav-link" href="#about">
+                About
+              </a>
+            </li>
+            <li
+              className={`nav-item ${activeSection === 'Projects' ? 'active' : ''}`}
+              onClick={() => handleNavigationClick('Projects')}
+            >
+              <a className="nav-link" href="#projects">
+                Projects
+              </a>
+            </li>
+            <li
+              className={`nav-item ${activeSection === 'Contact' ? 'active' : ''}`}
+              onClick={() => handleNavigationClick('Contact')}
+            >
+              <a className="nav-link" href="#contact">
+                Contact
+              </a>
+            </li>
+          </ul>
         </div>
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
-            <a className="nav-link" href="#about">About</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#projects">Projects</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#contact">Contact</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
-};
-
-export default Navigation;
+      </nav>
+    );
+  };
+  
+  export default Navigation;
 
 
