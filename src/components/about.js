a96fcf8f71c83b5f14e94d9fbd5fb681f48b4e18
JacobWillis <jacobwillis2012@gmail.com>
2023-06-14 20:37:56 -0400

    'added media query for about, project pages'

--- src/components/about.js
+++ src/components/about.js
@@ -3,14 +3,14 @@ import './about.css'
 
 const About = () => {
   return (
-    <div className='container-fluid p-2 m-2 d-flex align-items-center justify-content-center about-div animate__animated animate__fadeIn'>
-      <div className='row d-flex justify-content-center about-content'>
-        <p className='col-6 about-text bg-dark m-2'>
+    <div className='container-fluid d-flex align-items-center justify-content-center about-div animate__animated animate__fadeIn'>
+      <div className='row d-flex justify-content-center about-content p-3'>
+        <p className='col-sm-6 about-text bg-dark m-2 text-sm'>
           <h2 className='d-flex justify-content-center intro'>Hey, I'm Jacob Willis</h2> I'm an aspiring full stack developer! As a nerd, gamer, coach, and fitness enthusiast, I bring a unique perspective to my work.
           With a strong foundation in programming languages and frameworks, I'm continuously expanding my skills in the ever-evolving tech landscape. From front-end design to back-end development, 
           I'm eager to tackle new challenges and make meaningful contributions to innovative projects.
         </p>
-        <img className='col-6 profile-pic' alt="Its Me!" src="/images/profile-pic.jpg"></img>
+        <img className='col-sm-6 profile-pic' alt="Its Me!" src="/images/profile-pic.jpg"></img>
       </div>
     </div>
   );
