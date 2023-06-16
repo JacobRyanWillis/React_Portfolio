74077dfc371b745cbb2171ae9afa738f32887911
JacobWillis <jacobwillis2012@gmail.com>
2023-06-15 21:44:41 -0400

    'finishing touches for polish'

--- src/components/resume.js
+++ src/components/resume.js
@@ -6,7 +6,7 @@ const Resume = () => {
     return (
     <div className='container-fluid '>
         <div className='row resume-div'>
-            <div className='col-sm-6 mb-4 rounded border border-light text-center align-self-center bg-dark'>
+            <div className='col-sm-6 mb-4 rounded border border-light text-center align-self-center bg-dark animate__animated animate__bounceInLeft'>
                 <h2 className='text-primary underline'> Skills </h2>
                 <ul className='text-light list-unstyled underline'>
                     <li> REACT </li>
@@ -18,8 +18,9 @@ const Resume = () => {
                     <li> Hire me </li>
                 </ul>
             </div>
-            <div className='col-sm-6 text-center align-self-center'>
+            <div className='col-sm-6 text-center align-self-center animate__animated animate__bounceInRight'>
                 {/* <a href={JobResume} download><h2>Download</h2></a> */}
+                {/* I removed the download link^ because there is a download link in the iframe preview */}
                 <iframe className='resume-iframe' src={JobResume} title="My Resume"></iframe>
             </div>
         </div>
